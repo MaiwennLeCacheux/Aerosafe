6124ba5f9bfe8c639ff43198489118c5ef4fa54a
Bryan <46441004+bribrioche@users.noreply.github.com>
2021-11-07 11:59:18 +0100

    corrections de bug

--- app/src/main/java/com/example/aerosafe/map.java
+++ app/src/main/java/com/example/aerosafe/map.java
@@ -2,29 +2,68 @@ package com.example.aerosafe;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class map extends FragmentActivity implements OnMapReadyCallback {
+public class map extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
-    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient; // GPS
+    GoogleMap map; // map
     ArrayList<Airport> airports = new ArrayList<>();
+    double lat;
+    double lon;
+    // Flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // Flag for network status
+    boolean isNetworkEnabled = false;
+
+    // Flag for GPS status
+    boolean canGetLocation = false;
+
+    Location location; // Location
+    double latitude; // Latitude
+    double longitude; // Longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    LocationRequest locationRequest; //Demande de localisation
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,38 +72,127 @@ public class map extends FragmentActivity implements OnMapReadyCallback {
         ImageButton btnHome = findViewById(R.id.btnHome2);
         Intent intent = new Intent(this, MainActivity.class);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
         Intent ReIntent = getIntent();
 
         if (ReIntent != null) {
             if (ReIntent.hasExtra("BUNDLE")) {
                 Bundle args = ReIntent.getBundleExtra("BUNDLE");
                 airports = (ArrayList<Airport>) args.getSerializable("ARRAYLIST");
-                Log.d("liste ?", airports.get(0).icao);
+                //Log.d("liste ?", airports.get(0).icao);
             }
 
         }
 
+        getLocation();
+        if(location != null){
+            latitude = location.getLatitude();
+        }
+        if(location != null){
+            longitude = location.getLongitude();
+        }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         btnHome.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Bundle args = new Bundle();
-                args.putSerializable("ARRAYLIST",(Serializable)airports);
-                intent.putExtra("BUNDLE",args);
+                args.putSerializable("ARRAYLIST", (Serializable) airports);
+                intent.putExtra("BUNDLE", args);
                 startActivity(intent);
             }
         });
-
     }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+
+
+        Log.d("LAT/LONG 2", "onMapReady: " + latitude + " - " + longitude);
         map = googleMap;
 
-        LatLng Maharashtra = new LatLng(19.169257, 73.341601);
-        map.addMarker(new MarkerOptions().position(Maharashtra).title("Maharashtra"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Maharashtra));
+
+        LatLng posUser = new LatLng(latitude, longitude);
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.addMarker(new MarkerOptions().position(posUser).title("position de l'utilisateur"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(posUser));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                .zoom(10)                   // Sets the zoom
+                .bearing(0)                // Sets the orientation of the camera to east
+                .tilt(0)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+
+    public Location getLocation() {
+        try {
+            locationManager = (LocationManager) this
+                    .getSystemService(LOCATION_SERVICE);
+
+            // Getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // Getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // No network provider is enabled
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                                (android.location.LocationListener) this);
+                    }
+
+                    Log.d("Network", "Network");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // If GPS enabled, get latitude/longitude using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, (android.location.LocationListener) this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
     }
 }
